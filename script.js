22a87be32f5dad90820160f2b72cf1b71857bb9d
Wrongmanatee633 <55750647+Wrongmanatee633@users.noreply.github.com>
2022-11-28 23:09:25 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function iniciarMap(){
+    var coord = {lat:25.354826 ,lng:51.183884};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
+
